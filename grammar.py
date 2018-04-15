171a1a9b32342cbd86098849889e9ae55afff91c
Alyson Paige <alysonstiffel@gmail.com>
2018-04-15 13:34:35 -0400

    grammar notes

---
+++ grammar.py
@@ -0,0 +1,11 @@
+# Informal Python Notes
+
+# invalid syntax
+print 2 + 2 +
+
+# English:
+# sentence -> Subject Verb Object
+
+# Backus-Naur form
+# <non-terminal> -> replacement
+# Derivation, starting from some non-terminal. Follow the rules to derive a sequence of terminals. We're done when we have only terminals left. We can derive a sentence in the grammar.
