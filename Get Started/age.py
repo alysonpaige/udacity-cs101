a0cf1436a16591d4ca868adeb0e2e60571d80211
Alyson Paige <alysonstiffel@gmail.com>
2018-04-18 12:03:02 -0400

    age

---
+++ Get Started/age.py
@@ -0,0 +1,9 @@
+# Write python code that defines the variable 
+# age to be your age in years, and then prints 
+# out the number of days you have been alive.
+
+age = 26
+daysOfYear = 365
+totalDaysAlive = age * daysOfYear
+
+print totalDaysAlive
